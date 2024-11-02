9753931c5aae4fc19fd8ecb420142c887bdf215c
amolbinwade <amolbinwade@gmail.com>
2024-11-02 21:54:04 +0530

    solution for Isomorphic strings using map/arrays

---
+++ src/com/dsa/leetcodePractice/HashMap/IsomorphicStrings.java
@@ -0,0 +1,24 @@
+package com.dsa.leetcodePractice.HashMap;
+
+public class IsomorphicStrings {
+
+    public boolean isIsomorphic(String s, String t) {
+        int[] tMap = new int[128];
+        int[] sMap = new int[128];
+
+        for(int i=0; i<s.length(); i++){
+            char tChar = t.charAt(i);
+            char sChar = s.charAt(i);
+
+            sMap[sChar] = tChar;
+            tMap[tChar] = sChar;
+        }
+        for(int i=0; i<s.length(); i++){
+            char tChar = t.charAt(i);
+            char sChar = s.charAt(i);
+            if(tChar != sMap[sChar] || sChar != tMap[tChar])
+                return false;
+        }
+        return true;
+    }
+}
